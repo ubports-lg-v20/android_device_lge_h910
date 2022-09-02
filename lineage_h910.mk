de104234106bdd3556f975f86a6751c1705a9379
Walid Hammami <walid.ham@hotmail.com>
2022-09-02 21:20:04 +0100

    First change for halium
    Change-Id: I91a05fcb24576c2564ef7db88b39f94e726b2068

--- lineage_h910.mk
+++ lineage_h910.mk
@@ -16,10 +16,8 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/halium.mk)
 
 # Inherit from h910 device
 $(call inherit-product, device/lge/h910/device.mk)
